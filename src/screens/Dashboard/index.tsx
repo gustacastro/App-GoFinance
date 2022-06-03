191999ed6395cf0b080286f30ed41f958bffd45b
Gustavo Castro <gusncastro@gmail.com>
2022-06-03 03:50:31 -0300

    Created UserInfo and HighlightCards

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,68 @@
 import React from "react";
 
-import { Container, Title } from "./styles";
+import { HighlightCard } from "../../Components/HighlightCard";
+
+import {
+  Container,
+  Header,
+  UserInfo,
+  Photo,
+  User,
+  UserGretting,
+  UserName,
+  UserWrapper,
+  Icon,
+  HighlightCards,
+  Transactions,
+  Title,
+} from "./styles";
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/82110597?v=4",
+              }}
+            />
+
+            <User>
+              <UserGretting>Olá,</UserGretting>
+              <UserName>Gustavo</UserName>
+            </User>
+          </UserInfo>
+
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
+
+      <HighlightCards>
+        <HighlightCard
+          type="up"
+          title="Entradas"
+          amount="R$ 17.400,00"
+          lastTransaction="Última entrada dia 13 de abril"
+        />
+        <HighlightCard
+          type="down"
+          title="Saídas"
+          amount="R$ 1.259,00"
+          lastTransaction="Última entrada dia 03 de abril"
+        />
+        <HighlightCard
+          type="total"
+          title="Total"
+          amount="R$ 16.141,00"
+          lastTransaction="01 à 16 de abril"
+        />
+      </HighlightCards>
+
+      <Transactions>
+        <Title>Listagem</Title>
+      </Transactions>
     </Container>
   );
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,15 +1,88 @@
 import styled from "styled-components/native";
+import { AnyStyledComponent } from "styled-components";
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
+import { Feather } from "@expo/vector-icons";
+import Constants from "expo-constants";
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
-
   background-color: ${({ theme }) => theme.colors.background};
 `;
 
-export const Title = styled.Text`
+export const Header = styled.View`
+  width: 100%;
+  height: ${RFPercentage(42)}px;
+
+  background-color: ${({ theme }) => theme.colors.primary};
+
+  justify-content: center;
+  align-items: flex-start;
+  flex-direction: row;
+`;
+
+export const UserWrapper = styled.View`
+  width: 100%;
+  padding: 0 24px;
+  margin-top: ${RFValue(Constants.statusBarHeight) + 28}px;
+
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const UserInfo = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Icon = styled(Feather as unknown as AnyStyledComponent)`
+  color: ${({ theme }) => theme.colors.secondary};
+  font-size: ${RFValue(24)}px;
+`;
+
+export const Photo = styled.Image`
+  width: ${RFValue(55)}px;
+  height: ${RFValue(55)}px;
+
+  border-radius: 10px;
+`;
+
+export const User = styled.View`
+  margin-left: 17px;
+`;
+
+export const UserGretting = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+
+  font-size: ${RFValue(18)}px;
   font-family: ${({ theme }) => theme.fonts.regular};
-  font-size: 24px;
-  color: ${({ theme }) => theme.colors.title};
+`;
+
+export const UserName = styled.Text`
+  font-size: ${RFValue(18)}px;
+  color: ${({ theme }) => theme.colors.shape};
+  font-family: ${({ theme }) => theme.fonts.bold};
+`;
+
+export const HighlightCards = styled.ScrollView.attrs({
+  horizontal: true,
+  showsHorizontalScrollIndicator: false,
+  contentContainerStyle: { paddingHorizontal: 24 },
+})`
+  width: 100%;
+  position: absolute;
+
+  margin-top: ${RFPercentage(20)}px;
+`;
+
+export const Transactions = styled.View`
+  flex: 1;
+  padding: 0 24px;
+
+  margin-top: ${RFPercentage(12)}px;
+`;
+
+export const Title = styled.Text`
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
 `;
